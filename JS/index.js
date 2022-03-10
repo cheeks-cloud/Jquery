7a2aa79bd901a1203095f2ebce0fc696f24f519d
@cheeks cloud <agneskaswii700@gmail.com>
2022-03-10 16:56:01 +0300

    creating card with effects

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+  $('#thing').click(function(){
+    $('#some p').fadeIn('slow')
+  })
+})
+$(document).ready(function(){
+  $('#things').click(function(){
+    $('#somes ul').slideDown('slow')
+  })
+})
+$(document).ready(function(){
+$('#thing1').click(function(){
+  $('#somei p').slideUp('slow')
+})
+})
+$(document).ready(function(){
+  $('#thing2').click(function(){
+    $('#some1 p').fadeOut('slow')
+  })
+  })
+
+
